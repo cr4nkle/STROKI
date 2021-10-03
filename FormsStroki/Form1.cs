338538da8269fe125a2767c4b3adde3e816b0cf9
Сергей Попович <cr4nkle@yandex.ru>
2021-10-03 17:03:42 +0800

    Добавил код с логикой

--- FormsStroki/Form1.cs
+++ FormsStroki/Form1.cs
@@ -17,5 +17,45 @@ namespace FormsStroki
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Logic
+    {
+        public static int GetSum(string line)
+        {
+            char[] symbol = { '+', '-' };
+            string[] changeLine = line.Split(symbol);
+            int[] arr = new int[changeLine.Length];
+            int sum = int.Parse(changeLine[0]);
+
+            for (int i = 1; i < changeLine.Length; i++)
+            {
+                arr[i] = int.Parse(changeLine[i]);
+                if (i % 2 == 0)
+                {
+                    sum = sum - arr[i];
+                }
+                else
+                {
+                    sum = sum + arr[i];
+                }
+
+            }
+
+            return sum;
+        }
+
+        public static bool Check(string line)
+        {
+            bool check = true;
+            if (line == "")
+            {
+                check = false;
+            }
+            return check;
+        }
     }
 }
